3d1467cf02cf5bf1290b58194e6f3011a64b781a
Yuri Chub <gigikalo57@gmail.com>
2022-04-27 19:16:49 +0300

    change structure of the pug file

--- gulp/config/path.js
+++ gulp/config/path.js
@@ -7,7 +7,9 @@ const srcFolder = `./src`;
 
 export const path = {
     build: {
+        /********************** */
         html: `${buildFolder}/`,
+        /*********************** */  
         css: `${buildFolder}/css/`,
         js: `${buildFolder}/js/`,
         images: `${buildFolder}/img/`,
@@ -16,7 +18,9 @@ export const path = {
         files: `${buildFolder}/files/`
     },
     src: {
-        html: `${srcFolder}/*.pug`,
+        /************************** */
+        html: `${srcFolder}/pug/templates/*.pug`,
+        /************************** */
         scss: `${srcFolder}/scss/style.scss`,
         js: `${srcFolder}/js/app.js`,
         images: `${srcFolder}/img/**/*.{jpg,jpeg,png,gif,webp}`,
@@ -25,7 +29,9 @@ export const path = {
         files: `${srcFolder}/files/**/*.*`
     },
     watch: {
-        html:`${srcFolder}/**/*.pug`,
+        /************************ */
+        html:`${srcFolder}/pug/templates/**/*.pug`,
+        /************************ */
         scss: `${srcFolder}/scss/**/*.scss`,
         js: `${srcFolder}/js/**/*.js`,
         images: `${srcFolder}/img/**/*.{jpg,jpeg,png,gif,webp,svg,icon}`,
